ef4aa0d3909825ca3070bf973db64002424b4faa
Antonio Parrella <antonio.parrella@inbitcoin.it>
2017-07-12 15:07:58 +0200

    visiu: enabled webview remote debug and console log

--- app/src/main/java/com/greenaddress/greenbits/ui/VisiuWebview.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/VisiuWebview.java
@@ -2,13 +2,17 @@ package com.greenaddress.greenbits.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.ConsoleMessage;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -22,6 +26,7 @@ import static com.greenaddress.greenbits.ui.TabbedMainActivity.REQUEST_BITCOIN_U
  */
 
 public class VisiuWebview extends GaActivity {
+    private static final String TAG = VisiuWebview.class.getSimpleName();
     private View progressbarLayout;
     private Activity mActivity;
 
@@ -52,6 +57,26 @@ public class VisiuWebview extends GaActivity {
         progressbarLayout = UI.find(this, R.id.progressbarLayout);
 
         final WebView visiuWebview = UI.find(this, R.id.visiu_webview);
+
+        // enable remote debug on android >= 4.4
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))
+                WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        // log console errors
+        visiuWebview.setWebChromeClient(new WebChromeClient() {
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+                if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+                    Log.d(TAG, "[JS:" + cm.messageLevel().toString() + "] "
+                            + cm.message() + " -- From line "
+                            + cm.lineNumber() + " of "
+                            + cm.sourceId());
+                }
+                return true;
+            }
+        });
+
         final String url = "https://inbitcoin.it/static/visiu/index.html#" + publicAddress;
         visiuWebview.getSettings().setJavaScriptEnabled(true);
         visiuWebview.setWebViewClient(new CustomWebViewClient());
